2c825c985e62fa691b382458fb1a6aeb2bc44051
Dansot4891 <cnctlim94@gmail.com>
2025-04-17 16:29:33 +0900

    [feat] 로딩, 성공, 에러 basestate 정의

---
+++ lib/core/modules/state/base_state.dart
@@ -0,0 +1,1 @@
+enum BaseState { error, loading, success }
---
+++ lib/presentation/pages/search/photo_search_view_model.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter/foundation.dart';
+// import 'package:modu_3_hackathon/domain/use_case/get_photos_use_case.dart';
+
+// class PhotoSearchViewModel with ChangeNotifier {
+//   final GetPhotosUseCase _getPhotosUseCase;
+
+
+// }
