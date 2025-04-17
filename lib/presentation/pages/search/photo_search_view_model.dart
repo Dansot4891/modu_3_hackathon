d63e46117acc34c8362f07d9f2084f030002e603
Dansot4891 <cnctlim94@gmail.com>
2025-04-17 16:35:29 +0900

    [feat] 검색 화면 뷰 모델 구현 완료

--- lib/presentation/pages/search/photo_search_view_model.dart
+++ lib/presentation/pages/search/photo_search_view_model.dart
@@ -1,8 +1,30 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:modu_3_hackathon/domain/use_case/get_photos_use_case.dart';
+import 'package:flutter/foundation.dart';
+import 'package:modu_3_hackathon/core/modules/converter/tag_converter.dart';
+import 'package:modu_3_hackathon/core/modules/error_handling/result.dart';
+import 'package:modu_3_hackathon/core/modules/state/base_state.dart';
+import 'package:modu_3_hackathon/domain/model/photo.dart';
+import 'package:modu_3_hackathon/domain/use_case/get_photos_use_case.dart';
+import 'package:modu_3_hackathon/presentation/pages/search/photo_search_state.dart';
 
-// class PhotoSearchViewModel with ChangeNotifier {
-//   final GetPhotosUseCase _getPhotosUseCase;
+class PhotoSearchViewModel with ChangeNotifier {
+  final GetPhotosUseCase _getPhotosUseCase;
 
+  PhotoSearchViewModel(this._getPhotosUseCase);
 
-// }
+  PhotoSearchState _state = const PhotoSearchState();
+  PhotoSearchState get state => _state;
+
+  void getPhotos(String tags) async {
+    _state = state.copyWith(state: BaseState.error);
+    notifyListeners();
+    final result =
+        await _getPhotosUseCase.execute(CustomConverter.stringToList(tags));
+    switch (result) {
+      case Success<List<Photo>>():
+        _state = state.copyWith(state: BaseState.success, photos: result.data);
+      case Error<List<Photo>>():
+        _state = state.copyWith(state: BaseState.error);
+    }
+    notifyListeners();
+  }
+}
