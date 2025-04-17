f48acfa2c46c9191b5e9520307c5cd4d61983c8c
Dansot4891 <cnctlim94@gmail.com>
2025-04-17 17:45:30 +0900

    [feat] 상세 화면 state 정의

--- lib/presentation/pages/info/photo_info_state.dart
+++ lib/presentation/pages/info/photo_info_state.dart
@@ -1,5 +1,21 @@
-// import 'package:modu_3_hackathon/core/modules/state/base_state.dart';
+import 'package:modu_3_hackathon/core/modules/state/base_state.dart';
+import 'package:modu_3_hackathon/domain/info/model/photo_info.dart';
 
-// class PhotoInfoState {
-//   final BaseState state;
-// }
+class PhotoInfoState {
+  final BaseState state;
+  final PhotoInfo? photo;
+  const PhotoInfoState({
+    this.state = BaseState.init,
+    this.photo,
+  });
+
+  PhotoInfoState copyWith({
+    BaseState? state,
+    PhotoInfo? photo,
+  }) {
+    return PhotoInfoState(
+      state: state ?? this.state,
+      photo: photo ?? this.photo,
+    );
+  }
+}
