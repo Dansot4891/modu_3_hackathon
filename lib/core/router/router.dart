54cddc6f33614f29d4515cda694b7f6ab86c03e2
Dansot4891 <cnctlim94@gmail.com>
2025-04-17 16:00:59 +0900

    [feat] 앱 라우터 생성 및 설정 완료

---
+++ lib/core/router/router.dart
@@ -0,0 +1,4 @@
+import 'package:go_router/go_router.dart';
+import 'package:modu_3_hackathon/core/router/routes.dart';
+
+final router = GoRouter(routes: routes, initialLocation: AppRoutes.search.path);
